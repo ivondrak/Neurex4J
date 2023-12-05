5aff59622d3df9368c3e1807e6c61c8bad0d8fc2
Ivo Vondrak <ivo.vondrak@gmail.com>
2023-12-05 22:20:50 +0100

    neurex.png as a part of resources

--- src/neurex/gui/CredentialsPanel.java
+++ src/neurex/gui/CredentialsPanel.java
@@ -1,16 +1,29 @@
 package neurex.gui;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 public class CredentialsPanel extends JPanel {
+    BufferedImage img = null;
     public CredentialsPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        try {
+            InputStream stream = getClass().getResourceAsStream("/images/neurex.png");
+            img = ImageIO.read(Objects.requireNonNull(stream));
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
 
         JLabel titleLabel = new JLabel("Credentials");
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         titleLabel.setFont(new Font(titleLabel.getFont().getName(), Font.BOLD, 18));
 
-        ImageIcon imageIcon = new ImageIcon("Neurex.png");
+        ImageIcon imageIcon = new ImageIcon(img);
         JLabel imageLabel = new JLabel(imageIcon);
         imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
